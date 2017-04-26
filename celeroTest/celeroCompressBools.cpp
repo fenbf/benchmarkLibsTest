9c8a3c7bf7d1a66c89cdff4db877c9a3034c2bc5
Bartłomiej Filipek <joebaf@gmail.com>
2017-04-26 09:04:51 +0200

    clear warnings, set NoPacking version as the baseline, code cleanup, not working open mp version...

--- celeroTest/celeroCompressBools.cpp
+++ celeroTest/celeroCompressBools.cpp
@@ -1,4 +1,9 @@
+
+#pragma warning(push)
+#pragma warning(disable:4251) // warning C4251: celero::Result::pimpl': class 'celero::Pimpl<celero::Result::Impl>' needs to have dll-interface to be used by clients of class 'celero::Result'
 #include "celero\Celero.h"
+#pragma warning(pop)
+
 #include <bitset>
 #include <iostream>
 #include <random>
@@ -7,7 +12,7 @@ CELERO_MAIN;
 
 static const int SamplesCount = 5;
 static const int IterationsCount = 0;
-static const int ThresholdValue = 254;
+static const int ThresholdValue = 128;
 
 void Checker(bool val, bool reference, uint64_t i)
 {
@@ -37,25 +42,20 @@ public:
 	/// Before each run, build a vector of random integers.
 	virtual void setUp(int64_t experimentValue) override
 	{
-		inputValues.reset(new int[experimentValue]);
-		referenceValues.reset(new bool[experimentValue]);
-		arrayLength = experimentValue;
+		arrayLength = static_cast<size_t>(experimentValue);
+		inputValues.reset(new int[arrayLength]);
+		referenceValues.reset(new bool[arrayLength]);
+		
 
 		std::mt19937 gen(0); //Standard mersenne_twister_engine seeded with 0, constant
 		std::uniform_int_distribution<> dist(0, 255);
 
-		// set every byte
-		for (int64_t i = 0; i < experimentValue; ++i)
+		// set every byte, copute reference values
+		for (size_t i = 0; i < experimentValue; ++i)
 		{
 			inputValues[i] = dist(gen);
 			referenceValues[i] = inputValues[i] > ThresholdValue;
 		}
-	}
-
-	/// After each run, clear the vector
-	virtual void tearDown()
-	{
-		
 	}
 
 #ifdef _DEBUG
@@ -66,17 +66,44 @@ public:
 	static const int64_t BENCH_STEPS{ 5 };
 #endif
 	
-	int64_t arrayLength{ 0 };
+	size_t arrayLength{ 0 };
 	std::unique_ptr<int[]> inputValues;
 	std::unique_ptr<bool[]> referenceValues;
 };
+
+class NoPackingFixture : public CompressBoolsFixture
+{
+public:
+	virtual void setUp(int64_t experimentValue) override
+	{
+		CompressBoolsFixture::setUp(experimentValue);
+
+		outputValues.reset(new bool[static_cast<unsigned int>(arrayLength)]);
+	}
+
+	virtual void tearDown()
+	{
+		for (size_t i = 0; i < arrayLength; ++i)
+			Checker(outputValues[i], referenceValues[i], i);
+	}
+
+	std::unique_ptr<bool[]> outputValues;
+};
+
+BASELINE_F(CompressBoolsTest, NoPackingVersion, NoPackingFixture, SamplesCount, IterationsCount)
+{
+	for (size_t i = 0; i < arrayLength; ++i)
+	{
+		outputValues[i] = inputValues[i] > ThresholdValue;
+	}
+}
 
 class StdBitsetFixture : public CompressBoolsFixture
 {
 public:
 	virtual void tearDown()
 	{
-		for (int64_t i = 0; i < arrayLength; ++i)
+		for (size_t i = 0; i < arrayLength; ++i)
 			Checker(outputBitset[i], referenceValues[i], i);
 	}
 
@@ -85,9 +112,9 @@ public:
 
 // Run an automatic baseline.  
 // Celero will help make sure enough samples are taken to get a reasonable measurement
-BASELINE_F(CompressBoolsTest, StdBitset, StdBitsetFixture, SamplesCount, IterationsCount)
+BENCHMARK_F(CompressBoolsTest, StdBitset, StdBitsetFixture, SamplesCount, IterationsCount)
 {
-	for (int64_t i = 0; i < arrayLength; ++i)
+	for (size_t i = 0; i < arrayLength; ++i)
 		outputBitset.set(i, inputValues[i] > ThresholdValue);
 }
 
@@ -98,12 +125,12 @@ public:
 	{
 		CompressBoolsFixture::setUp(experimentValue);
 
-		outputVector.resize(experimentValue);
+		outputVector.resize(static_cast<unsigned int>(experimentValue));
 	}
 
 	virtual void tearDown()
 	{
-		for (int64_t i = 0; i < arrayLength; ++i)
+		for (size_t i = 0; i < arrayLength; ++i)
 			Checker(outputVector[i], referenceValues[i], i);
 	}
 
@@ -112,7 +139,7 @@ public:
 
 BENCHMARK_F(CompressBoolsTest, StdVector, StdVectorFixture, SamplesCount, IterationsCount)
 {
-	for (int64_t i = 0; i < arrayLength; ++i)
+	for (size_t i = 0; i < arrayLength; ++i)
 		outputVector[i] = inputValues[i] > ThresholdValue;
 }
 
@@ -123,14 +150,14 @@ public:
 	{
 		CompressBoolsFixture::setUp(experimentValue);
 
-		numBytes = (experimentValue + 7) / 8;
-		numFullBytes = (experimentValue) / 8;
+		numBytes = static_cast<unsigned int>((experimentValue + 7) / 8);
+		numFullBytes = static_cast<unsigned int>((experimentValue) / 8);
 		outputValues.reset(new uint8_t[numBytes]);
 	}
 
 	virtual void tearDown()
 	{
-		for (int64_t i = 0; i < arrayLength; ++i)
+		for (size_t i = 0; i < arrayLength; ++i)
 			Checker(getBoolAt(i), referenceValues[i], i);
 	}
 
@@ -141,8 +168,8 @@ public:
 		return (*(outputValues.get() + bytePos)) & (1 << bitPos);
 	}
 
-	int64_t numBytes;
-	int64_t numFullBytes;
+	unsigned int numBytes;
+	unsigned int numFullBytes;
 	std::unique_ptr<uint8_t[]> outputValues;
 };
 
@@ -178,6 +205,130 @@ BENCHMARK_F(CompressBoolsTest, SingleVarVersion, ManualVersionFixture, SamplesCo
 }
 
 BENCHMARK_F(CompressBoolsTest, NotDepentendVersion, ManualVersionFixture, SamplesCount, IterationsCount)
+{
+	uint8_t Bits[8] = { 0 };
+	const int64_t lenDivBy8 = (arrayLength / 8) * 8;
+
+	auto pInputData = inputValues.get();
+	auto pOutputByte = outputValues.get();
+
+	for (int64_t j = 0; j < lenDivBy8; j += 8)
+	{
+		Bits[0] = pInputData[0] > ThresholdValue ? 0x01 : 0;
+		Bits[1] = pInputData[1] > ThresholdValue ? 0x02 : 0;
+		Bits[2] = pInputData[2] > ThresholdValue ? 0x04 : 0;
+		Bits[3] = pInputData[3] > ThresholdValue ? 0x08 : 0;
+		Bits[4] = pInputData[4] > ThresholdValue ? 0x10 : 0;
+		Bits[5] = pInputData[5] > ThresholdValue ? 0x20 : 0;
+		Bits[6] = pInputData[6] > ThresholdValue ? 0x40 : 0;
+		Bits[7] = pInputData[7] > ThresholdValue ? 0x80 : 0;
+
+		*pOutputByte++ = Bits[0] | Bits[1] | Bits[2] | Bits[3] | Bits[4] | Bits[5] | Bits[6] | Bits[7];
+		pInputData += 8;
+	}
+	if (arrayLength & 7)
+	{
+		auto RestW = arrayLength & 7;
+		memset(Bits, 0, 8);
+		for (long long i = 0; i < RestW; ++i)
+		{
+			Bits[i] = *pInputData == ThresholdValue ? 1 << i : 0;
+			pInputData++;
+		}
+		*pOutputByte++ = Bits[0] | Bits[1] | Bits[2] | Bits[3] | Bits[4] | Bits[5] | Bits[6] | Bits[7];
+	}
+}
+
+struct bool8 {
+	uint8_t val0 : 1;
+	uint8_t val1 : 1;
+	uint8_t val2 : 1;
+	uint8_t val3 : 1;
+	uint8_t val4 : 1;
+	uint8_t val5 : 1;
+	uint8_t val6 : 1;
+	uint8_t val7 : 1;
+};
+
+class PackedStructFixture : public CompressBoolsFixture
+{
+public:
+	virtual void setUp(int64_t experimentValue) override
+	{
+		CompressBoolsFixture::setUp(experimentValue);
+
+		numBytes = static_cast<unsigned int>((experimentValue + 7) / 8);
+		numFullBytes = static_cast<unsigned int>((experimentValue) / 8);
+		outputValues.reset(new bool8[numBytes]);
+	}
+
+	virtual void tearDown()
+	{
+		for (size_t i = 0; i < arrayLength; ++i)
+			Checker(getBoolAt(i), referenceValues[i], i);
+	}
+
+	bool getBoolAt(int64_t pos)
+	{
+		auto bytePos = pos / 8;
+		auto bitPos = pos % 8;
+		switch (bitPos)
+		{
+		case 0:return (*(outputValues.get() + bytePos)).val0;
+		case 1:return (*(outputValues.get() + bytePos)).val1;
+		case 2:return (*(outputValues.get() + bytePos)).val2;
+		case 3:return (*(outputValues.get() + bytePos)).val3;
+		case 4:return (*(outputValues.get() + bytePos)).val4;
+		case 5:return (*(outputValues.get() + bytePos)).val5;
+		case 6:return (*(outputValues.get() + bytePos)).val6;
+		}
+		return (*(outputValues.get() + bytePos)).val7;
+	}
+
+	unsigned int numBytes;
+	unsigned int numFullBytes;
+	std::unique_ptr<bool8[]> outputValues;
+};
+
+BENCHMARK_F(CompressBoolsTest, PackedStructVersion, PackedStructFixture, SamplesCount, IterationsCount)
+{
+	const int64_t lenDivBy8 = (arrayLength / 8) * 8;
+
+	auto pInputData = inputValues.get();
+	auto pOutputByte = outputValues.get();
+
+	bool8 out;
+
+	for (int64_t j = 0; j < lenDivBy8; j += 8)
+	{
+		out.val0 = pInputData[0] > ThresholdValue;
+		out.val1 = pInputData[1] > ThresholdValue;
+		out.val2 = pInputData[2] > ThresholdValue;
+		out.val3 = pInputData[3] > ThresholdValue;
+		out.val4 = pInputData[4] > ThresholdValue;
+		out.val5 = pInputData[5] > ThresholdValue;
+		out.val6 = pInputData[6] > ThresholdValue;
+		out.val7 = pInputData[7] > ThresholdValue;
+
+		*pOutputByte++ = out;
+		pInputData += 8;
+	}
+	if (arrayLength & 7)
+	{
+		auto RestW = arrayLength & 7;
+		out = { 0, 0, 0, 0, 0, 0, 0, 0 };
+		if (RestW > 6) out.val6 = pInputData[6] > ThresholdValue;
+		if (RestW > 5) out.val5 = pInputData[5] > ThresholdValue;
+		if (RestW > 4) out.val4 = pInputData[4] > ThresholdValue;
+		if (RestW > 3) out.val3 = pInputData[3] > ThresholdValue;
+		if (RestW > 2) out.val2 = pInputData[2] > ThresholdValue;
+		if (RestW > 1) out.val1 = pInputData[1] > ThresholdValue;
+		if (RestW > 0) out.val0 = pInputData[0] > ThresholdValue;
+		*pOutputByte++ = out;
+	}
+}
+
+BENCHMARK_F(CompressBoolsTest, WithOpenMP, ManualVersionFixture, SamplesCount, IterationsCount)
 {
 	uint8_t Bits[8] = { 0 };
 	const int64_t lenDivBy8 = (arrayLength / 8) * 8;
